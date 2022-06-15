591a6016cbfb5faf3ad1ad897a387320ad105054
unknown <abhipsaguru@gmail.com>
2022-06-15 23:59:22 +0530

    day 29

--- Day 29/partition-list.cpp
+++ Day 29/partition-list.cpp
@@ -40,3 +40,45 @@ public:
         return head;
     }
 };
+
+//another sol:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        vector<int> v;
+        vector<int> v1;
+        ListNode* temp=head;
+        ListNode* temp1=head;
+        while(temp!=NULL){
+            if(temp->val<x){
+                v.push_back(temp->val);
+            }else{
+                v1.push_back(temp->val);
+            }
+            temp=temp->next;
+        }
+        int i=0;
+        while(temp1!=NULL && i<v.size()){
+            temp1->val=v[i];
+            temp1=temp1->next;
+            i++;
+        }
+        int j=0;
+        while(temp1!=NULL){
+            temp1->val=v1[j];
+            j++;
+            temp1=temp1->next;
+        }
+        return head;
+    }
+};
